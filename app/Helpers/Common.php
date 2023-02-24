edf323146317026cfa93860a31a2694eb52cdba1
tranvannghia021 <nghiatv1@fireapps.vn>
2023-02-24 16:53:02 +0700

    pusher

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -2,8 +2,21 @@
 namespace App\Helpers;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use Pusher\Pusher;
 
 class Common{
+
+   public static function pushSocket(string $channel, string $event, array $payload)
+    {
+        $pusher = new Pusher(
+            config('services.pusher.key'),
+            config('services.pusher.secret'),
+            config('services.pusher.app_id'),
+            config('services.pusher.options')
+        );
+        $pusher->trigger($channel, $event, $payload);
+    }
+
     public static function encodeJWT(array $payload) :string{
         $payload['expire']=date("Y-m-d H:i:s",time() + config('auth.key.expire'));
         return JWT::encode($payload, config('auth.key.jwt'),config('auth.key.alg'));
